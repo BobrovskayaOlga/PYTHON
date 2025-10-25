1b1f2e3d702b67306ebf298592703b32edc4ae13
BobrovskayaOlga <OLGANOV13.246@gmail.com>
2025-10-25 14:58:18 +0300

    06K_lesson

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -3,41 +3,44 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()
 
-try:
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-    
-    delay_input = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "#delay"))
-    )
-    delay_input.clear()
-    delay_input.send_keys("45")
-    
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//span[text()='7']"))
-    ).click()
-    
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//span[text()='+']"))
-    ).click()
-    
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//span[text()='8']"))
-    ).click()
-    
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//span[text()='=']"))
-    ).click()
-    
-    WebDriverWait(driver, 50).until(
-        EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15")
-    )
-    
-    screen_element = driver.find_element(By.CLASS_NAME, "screen")
-    result_text = screen_element.text
-    
-    assert result_text == "15"
+def test_slow_calculator():
+    """Тест проверяет работу калькулятора с задержкой"""
+    driver = webdriver.Chrome()
 
-finally:
-    driver.quit()
+    try:
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+        
+        delay_input = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#delay"))
+        )
+        delay_input.clear()
+        delay_input.send_keys("45")
+        
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[text()='7']"))
+        ).click()
+        
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[text()='+']"))
+        ).click()
+        
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[text()='8']"))
+        ).click()
+        
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[text()='=']"))
+        ).click()
+        
+        WebDriverWait(driver, 50).until(
+            EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15")
+        )
+        
+        screen_element = driver.find_element(By.CLASS_NAME, "screen")
+        result_text = screen_element.text
+        
+        assert result_text == "15"
+
+    finally:
+        driver.quit()
