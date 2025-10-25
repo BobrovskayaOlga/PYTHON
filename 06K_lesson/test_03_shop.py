a9e01a8d87898e43eb7e1041248d00ac9133c8c7
BobrovskayaOlga <OLGANOV13.246@gmail.com>
2025-10-25 15:43:15 +0300

    06K_lesson/test_03_shop.py

--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -1,47 +1,72 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_slow_calculator():
-    """Тест проверяет работу калькулятора с задержкой"""
-    driver = webdriver.Chrome()
+def test_saucedemo_total_price():
+    """Тест проверяет корректность итоговой суммы в интернет-магазине"""
+    firefox_options = Options()
+    driver = webdriver.Firefox(options=firefox_options)
 
     try:
-        driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-        
-        delay_input = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "#delay"))
-        )
-        delay_input.clear()
-        delay_input.send_keys("45")
+        driver.get("https://www.saucedemo.com/")
 
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//span[text()='7']"))
-        ).click()
-        
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//span[text()='+']"))
-        ).click()
-        
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//span[text()='8']"))
-        ).click()
-        
-        WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.XPATH, "//span[text()='=']"))
-        ).click()
-        
-        WebDriverWait(driver, 50).until(
-            EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15")
+
+        username_field = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "user-name"))
         )
-        
-        screen_element = driver.find_element(By.CLASS_NAME, "screen")
-        result_text = screen_element.text
-        
-        assert result_text == "15"
+        password_field = driver.find_element(By.ID, "password")
+        login_button = driver.find_element(By.ID, "login-button")
+
+        username_field.send_keys("standard_user")
+        password_field.send_keys("secret_sauce")
+        login_button.click()
+
+        backpack_add = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "add-to-cart-sauce-labs-backpack"))
+        )
+        backpack_add.click()
+
+        tshirt_add = driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt")
+        tshirt_add.click()
+
+        onesie_add = driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie")
+        onesie_add.click()
+
+        cart_icon = driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+        cart_icon.click()
+
+        checkout_button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "checkout"))
+        )
+        checkout_button.click()
+
+        first_name_field = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "first-name"))
+        )
+        first_name_field.send_keys("Ольга")
+
+        last_name_field = driver.find_element(By.ID, "last-name")
+        last_name_field.send_keys("Бобровская")
+
+        postal_code_field = driver.find_element(By.ID, "postal-code")
+        postal_code_field.send_keys("309501")
+
+        continue_button = driver.find_element(By.ID, "continue")
+        continue_button.click()
+
+        total_element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "summary_total_label"))
+        )
+        total_text = total_element.text
+        total_value = total_text.replace("Total: $", "")
+
+        expected_total = "$58.29"
+        actual_total = f"${total_value}"
+
+        assert actual_total == expected_total, f"Ожидалось {expected_total}, получено {actual_total}"
 
     finally:
         driver.quit()
